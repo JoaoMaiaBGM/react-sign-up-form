6e368f0411d03be2325ce5b2b96dc6981c190669
João Maia <102671695+JoaoMaiaBGM@users.noreply.github.com>
2022-08-09 12:00:15 -0300

    style: render toastify pop-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import { ToastContainer } from "react-toastify";
 
+import "react-toastify/dist/ReactToastify.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="main">
+        <ToastContainer autoClose={3000} />
         <Routes>
-          <Route path="/dashboard/:id" />
-          <Route path="/signup/" />
-          <Route path="/" />
+          <Route path="/dashboard/:id" element={<Dashboard />} />
+          <Route path="/signup/" element={<SignUp />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </div>
     </div>
